d191f8c4849a1c7d052a8d6d88dcb8b974346e6a
DanielPaulGriffin <daniel.paul.griffin@gmail.com>
2020-08-10 21:42:26 +1000

    first day

---
+++ RPG Construction Kit/Assets/Scripts/Journal.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Journal : MonoBehaviour,ICondition<int>
+{
+    public List<JournalEntry> journalEntries;
+    public bool CheckCondition(int index)
+    {
+        return true;
+    }
+
+    public void AddJournalEntry()
+    {
+
+    }
+}
+
+public class JournalEntry
+{
+    public List<Entry> entries;
+    public string id;
+}
+
+public class Entry
+{
+   public string entryText;
+   public int index;
+   public List<string> conditions;
+}
