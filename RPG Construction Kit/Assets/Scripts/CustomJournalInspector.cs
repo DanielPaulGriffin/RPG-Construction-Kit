22db5ebaa7a3ea04d4badf575b9dc6a52b5a8df4
DanielPaulGriffin <daniel.paul.griffin@gmail.com>
2020-08-11 09:38:40 +1000

    little progress

---
+++ RPG Construction Kit/Assets/Scripts/CustomJournalInspector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Journal))]
+public class CustomJournalInspector : Editor
+{
+    string journalID;
+    public override void OnInspectorGUI()
+    {
+        //serializedObject.Update();
+        EditorGUILayout.LabelField("Journal Editor - DPG");
+        DrawDefaultInspector();
+        Journal journal = (Journal)target;
+        
+        
+        journalID = EditorGUILayout.TextField("Journal ID:",journalID );
+        
+        if (GUILayout.Button("Create New Entry"))
+        {
+            GUIUtility.keyboardControl = 0;
+           // EditorUtility.SetDirty(this);
+           // serializedObject.ApplyModifiedProperties();
+           // this.Repaint();
+            journal.AddJournalEntry(journalID);
+            
+        }
+      //  serializedObject.ApplyModifiedProperties();
+    }
+}
--- RPG Construction Kit/Assets/Scripts/Journal.cs
+++ RPG Construction Kit/Assets/Scripts/Journal.cs
@@ -2,29 +2,46 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Journal : MonoBehaviour,ICondition<int>
 {
+    
     public List<JournalEntry> journalEntries;
+
+    public void Start()
+    {
+        
+    }
     public bool CheckCondition(int index)
     {
         return true;
     }
 
-    public void AddJournalEntry()
+    public void AddJournalEntry(string id)
     {
-
+        if (journalEntries==null)
+        {
+            journalEntries = new List<JournalEntry>();
+        }
+        JournalEntry je = new JournalEntry();
+       
+        je.id = id;
+        journalEntries.Add(je);
+       // je.entries = new List<Entry>();
     }
 }
 
+[System.Serializable]
 public class JournalEntry
 {
-    public List<Entry> entries;
     public string id;
+    public List<Entry> entries;
 }
 
+[System.Serializable]
 public class Entry
 {
    public string entryText;
    public int index;
-   public List<string> conditions;
+  // public List<string> conditions;
 }
